66b63bad2127022b9372fc9af1ec44c18536d02e
Hüseyin TÖREMEN <huseyintoremen@gmail.com>
2024-11-08 11:55:19 +0300

    Added ocelot

--- apigateways/OcelotApigateway/Program.cs
+++ apigateways/OcelotApigateway/Program.cs
@@ -1,8 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddHealthChecks();
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddHealthChecks()
+    .AddUrlGroup(new Uri("https://localhost:7101/healthz"), "IdentityService")
+    .AddUrlGroup(new Uri("https://localhost:7102/healthz"), "AuthService");
+
 
 var app = builder.Build();
 
@@ -11,4 +18,5 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseHealthChecks("/healthz");
+await app.UseOcelot();
 app.Run();
