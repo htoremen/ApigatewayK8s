243cc701f088607b6487b3aa80973ce5455a40ba
Hüseyin TÖREMEN <huseyintoremen@gmail.com>
2024-11-08 16:41:08 +0300

    Updated docker-compose

--- apigateways/OcelotApigateway/Program.cs
+++ apigateways/OcelotApigateway/Program.cs
@@ -6,9 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddOcelot(builder.Configuration);
-builder.Services.AddHealthChecks()
-    .AddUrlGroup(new Uri("https://localhost:7101/healthz"), "IdentityService")
-    .AddUrlGroup(new Uri("https://localhost:7102/healthz"), "AuthService");
+builder.Services.AddHealthChecks();
+    //.AddUrlGroup(new Uri("https://localhost:7101/healthz"), "identityservice")
+    //.AddUrlGroup(new Uri("https://localhost:7102/healthz"), "authservice");
 
 
 var app = builder.Build();
